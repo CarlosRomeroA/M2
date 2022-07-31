fddd37a9c9eefb62240a80afd1f14d555e4bf0e4
CarlosRomeroA <carlos.romero.aguirre@gmail.com>
2022-07-31 01:01:03 -0400

    Homework Ajax 2

--- FT-M2-master/04-Ajax/homework/index.js
+++ FT-M2-master/04-Ajax/homework/index.js
@@ -1,12 +1,45 @@
-$("#boton").click(()=> {
-    $("#lista").empty();
+// const { get } = require("@11ty/eleventy/src/TemplateCache");
+
+$("#boton").click( ()=> {
+    $("#lista").empty()
     $.ajax({
         url: 'http://localhost:5000/amigos',
         type: 'get',
         success: (info) => {
-            info.forEach((amigo) => $("#lista").append(`<li id=${amigo.id}> ${amigo.name}</li>`));
+            info.forEach((amigo) => $("#lista").append(`<li id=${amigo.id}> ${amigo.name}</li>`))
         }
-
     })
 })
 
+$("#search").click(()=> {
+    $("#amigo").empty();
+    let id = $("#input").val();
+    $.ajax({
+        url: `http://localhost:5000/amigos/${id}`,
+        type: 'get',
+        success: (info) => {
+            $("#amigo").append(`<span>${info.name}</span>`);
+            $("#input").val("");
+        }
+    })
+})
+
+$("#delete").click( () => {
+    let id = $("#inputDelete").val();
+    $.ajax({
+        url: `http://localhost:5000/amigos/${id}`,
+        type: "delete",
+        success: (info) => {
+            alert("amigo eliminado con exito");
+            $("#inputDelete").val("");
+            $("#lista").empty();
+            $.ajax({
+                url: 'http://localhost:5000/amigos',
+                type: 'get',
+                success: (info) => {
+                    info.forEach((amigo) => $("#lista").append(`<li id=${amigo.id}> ${amigo.name}</li>`))
+                }
+            })
+        }
+    })
+})
