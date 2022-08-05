d54c85384e6e81d7cb5793955a97619e9800a36c
CarlosRomeroA <carlos.romero.aguirre@gmail.com>
2022-08-05 00:18:10 -0400

    Homework Bundlers

--- FT-M2-master/05-Bundlers/homework/webpack.config.js
+++ FT-M2-master/05-Bundlers/homework/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: './browser/app.js', 
+    entry: './browser/app.js', // el punto de arranque de nuestro programa
     output: {
       path: __dirname + '/browser', // el path absoluto para el directorio donde queremos que el output sea colocado
       filename: 'bundle.js' // el nombre del archivo que va a contener nuestro output - podemos nombrarlo como queramos pero bundle.js es lo típico
