b90f307591ec3587d1dd2f0a3de50a13f4bcec90
CarlosRomeroA <carlos.romero.aguirre@gmail.com>
2022-08-14 23:42:50 -0400

    Homework React-Estilos

--- FT-M2-master/07-React-Estilos/homework/src/App.js
+++ FT-M2-master/07-React-Estilos/homework/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
+import cities from './data.js';
 import data, { Cairns } from './data.js';
 
 function App() {
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <Card
           max={Cairns.main.temp_max}
           min={Cairns.main.temp_min}
           name={Cairns.name}
           img={Cairns.weather[0].icon}
-          onClose={() => alert(Cairns.name)}
+          onClose={() => alert('Bazinga!')}
         />
       </div>
-      <hr />
+      <hr /> */}
       <div>
         <Cards
           cities={data}
--- FT-M2-master/07-React-Estilos/homework/src/components/Cards.jsx
+++ FT-M2-master/07-React-Estilos/homework/src/components/Cards.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Card from './Card';
+import s from './Cards.module.css';
 
-export default function Cards(props) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  return (
+    <div className={s.container}>
+      {
+        cities.map(ciudad => 
+          <Card
+            max={ciudad.main.temp_max}
+            min={ciudad.main.temp_min}
+            name={ciudad.name}
+            img={ciudad.weather[0].icon}
+            onClose={() => alert('Bazinga2!')}
+          />
+        )
+      }
+    </div>  
+  )
 };
+console.log(Card);
